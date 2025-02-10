726ffae2941663e3e0af01990d39f710499a8dce
polandmangoes12 <60554971+polandmangoes12@users.noreply.github.com>
2025-02-10 02:12:33 +0000

    comments

--- project2.cpp
+++ project2.cpp
@@ -1,3 +1,14 @@
+/************************************************************/
+/* Author: Michelle Kilgallen */
+/* Major: non-degree */
+/* Creation Date: February 8, 2025 */
+/* Due Date: February 13, 2025 */
+/* Course: CSC 402 */
+/* Professor Name: Dr. Zhang */
+/* Assignment: Project 2 */
+/* Filename: project2_MichelleKilgallen.cpp */
+/* Purpose: The program will ask the user for a sequence of integers, use them to build an AVL tree, and print the total number of nodes, pre-order, post-order traversal sequence of the tree. The test program will then test the findkth() method, which will take the root node and an integer k as inputs and return the k’th smallest element in the tree. */
+/************************************************************/
 #include <iostream>
 #include "AvlTree.h"
 using namespace std;
